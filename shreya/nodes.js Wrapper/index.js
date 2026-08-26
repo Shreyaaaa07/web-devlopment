e4c7df06f67e51cb08e8e0a38ff8c12235f517ab
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-08-26 23:30:43 +0530

    added node js file

--- shreya/nodes.js Wrapper/index.js
+++ shreya/nodes.js Wrapper/index.js
@@ -5,3 +5,9 @@ console.log(__filename, __dirname)
 (function(exports, require, module, __filename, __dirname) {
   // Your entire file code lives here
 });
+
+console.log(__filename);
+// Example: /Users/haris/project/app.js
+
+console.log(__dirname);
+// Example: /Users/haris/project
