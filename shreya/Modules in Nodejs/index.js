150f620d0b72ffa04c8b637616d582a4c9f8fca0
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-08-26 21:09:20 +0530

    added node js file

--- shreya/Modules in Nodejs/index.js
+++ shreya/Modules in Nodejs/index.js
@@ -11,6 +11,12 @@ module.exports = {add, weirdFunction}
 // app.js
 const math = require('./math');
 console.log(math.add(5, 3));
-// app.js
-const math = require('./math');
-console.log(math.add(5, 3));
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000);
