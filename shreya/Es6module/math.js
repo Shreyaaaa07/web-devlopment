b1ccc6aaa268cb0e5400a573f7b7f55bd3e5aada
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-08-26 21:58:25 +0530

    added node js file

---
+++ shreya/Es6module/math.js
@@ -0,0 +1,14 @@
+// math.js
+function add(a, b){
+    return a + b;
+}
+module.exports = {add};
+
+// app.js
+const math = required('./math');
+console.log(math.add(2, 3));
+
+// math.mjs
+export function add(a, b) {
+  return a + b;
+}
