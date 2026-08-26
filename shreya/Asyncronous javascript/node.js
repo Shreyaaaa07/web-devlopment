4fbc0f5eb2f913ad85b731b2e08650926fd0ddd7
Shreyaaaa07 <shreyabhosale711@gmail.com>
2026-08-26 23:34:15 +0530

    added node js file

---
+++ shreya/Asyncronous javascript/node.js
@@ -0,0 +1,16 @@
+// console.log("A");
+// console.log("B");
+// console.log("C");
+
+console.log("A");
+
+setTimeout(() => {
+  console.log("B");
+}, 1000);
+
+console.log("C");
+
+// Output:
+// A
+// C
+// B (after about 1 second)
